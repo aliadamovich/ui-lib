39f7550052a8437629f69bdffb342b2a212088f1
aliadamovich <alesya.argentina@gmail.com>
2025-07-10 16:31:39 -0300

    stylelint

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,3 +1,34 @@
 module.exports = {
-    extends: '@it-incubator/stylelint-config',
+  extends: [
+    '@it-incubator/stylelint-config',
+    'stylelint-config-standard',
+  ],
+  plugins: ['stylelint-order', 'stylelint-scss'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'selector-class-pattern': null,
+    "selector-pseudo-class-no-unknown": [true, {
+      ignorePseudoClasses: ["global"]
+    }],
+    'order/properties-order': [
+      'position',
+      'top',
+      'right',
+      'bottom',
+      'left',
+      'z-index',
+      'display',
+      'flex',
+      'align-items',
+      'justify-content',
+      'width',
+      'height',
+      'padding',
+      'margin',
+      'font-size',
+      'color',
+      'background',
+    ],
+  },
 }
